33841b783de0a424af7a2ee69f6392895c1e8509
cbidici <coskun.bidici@udemy.com>
2020-05-03 21:27:18 +0300

    Fix fields of post list

--- blog/serializers.py
+++ blog/serializers.py
@@ -3,9 +3,18 @@ from rest_framework import serializers
 from .models import Post
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Post
-        fields = ['url', 'id', 'owner', 'title', 'text', 'created']
+        fields = ['url', 'id', 'owner', 'title', 'summary', 'text', 'created']
+
+
+class PostListSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Post
+        fields = ['url', 'id', 'owner', 'title', 'summary', 'created']
+        read_only_fields = ['url', 'id', 'owner', 'title', 'summary', 'created']
--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,21 @@ from rest_framework import viewsets
 
 from .models import Post
 from .permissions import IsOwnerOrReadOnlyObject
-from .serializers import PostSerializer
+from .serializers import PostListSerializer, PostSerializer
 
 
-class PostViewSet(viewsets.ModelViewSet):
+class MultiSerializerViewSetMixin:
+    def get_serializer_class(self):
+        return self.serializers.get(self.action, self.serializer_class)
+
+
+class PostViewSet(MultiSerializerViewSetMixin, viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnlyObject]
-    serializer_class = PostSerializer
     queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    serializers = {
+        'list': PostListSerializer
+    }
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
